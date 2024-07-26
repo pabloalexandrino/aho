07821317114ef587502374c3a7efa3190d9cc2f2
Pablo Alexandrino <pabloalexandrino@icloud.com>
2024-07-25 22:02:22 -0300

    added select theme for embed form

--- src/pages/form_embed.tsx
+++ src/pages/form_embed.tsx
@@ -5,11 +5,6 @@ import 'react-toastify/dist/ReactToastify.css'
 import { InputMask } from '@react-input/mask'
 import fb from '../utils/fb'
 import { OfferContext } from '../providers/Offer'
-import BlurImage from '../components/BlurImage'
-import Geo from '../components/Geo'
-import VTurb from '../components/Player/VTurb'
-import Blur from '../components/Blur'
-import FloatBlocks from '../components/FloatBlocks'
 
 export default function Embed() {
     const {
@@ -40,6 +35,7 @@ export default function Embed() {
 
         const urlParams = new URLSearchParams(window.location.search)
         const tag = urlParams.get('tag') ?? ''
+        setTheme(urlParams.get('theme') ?? 'dark')
         setPagina(tag)
 
     }, [eventId, setPagina])
@@ -50,7 +46,7 @@ export default function Embed() {
     // const [instagram2, setInstagram2] = useState('')
     const [renda, setRenda] = useState('')
     const [loading2, setLoading2] = useState(false)
-
+    const [theme, setTheme] = useState('dark')
     const schema = yup.object().shape({
         nameLead2: yup.string().required('Por favor, informe o seu nome.'),
         emailLead2: yup
@@ -142,119 +138,236 @@ export default function Embed() {
     }
 
     return (
-        <div className={'bg-transparent'}>
-            <ToastContainer />
-            <div className='w-full max-w-2xl text-left flex flex-col mx-auto'>
-                <h2 className='font-light text-white mt-4 text-xs sm:text-base'>
-                    Preencha seus dados abaixo para receber a apresentação completa da <span
-                    className='font-bold'>Agência de Viagens Home Office</span> ✌
-                </h2>
-                <div className='card card-compact border w-full h-fit rounded-lg my-4 md:my-12'>
-                    <div className='card-body'>
-                        <div className='card-title text-primary'>Inscreva-se</div>
+         theme === 'dark' ? (
+            <div className={'bg-black h-screen'}>
+                <ToastContainer />
+                <div className='w-full max-w-2xl text-left flex flex-col mx-auto'>
+                    <h2 className='font-light text-white mt-4 text-xs sm:text-base'>
+                        Preencha seus dados abaixo para receber a apresentação completa da <span
+                        className='font-bold'>Agência de Viagens Home Office</span> ✌
+                    </h2>
+                    <div className='card card-compact border w-full h-fit rounded-lg my-4 md:my-12'>
+                        <div className='card-body'>
+                            <div className='card-title text-primary'>Inscreva-se</div>
 
-                        <form
-                            id='formLead'
-                            className='py-4 grid gap-2'
-                            onSubmit={handleSubmitLead}
-                        >
-                            <input
-                                type='text'
-                                name='name_lead'
-                                placeholder='Nome Completo'
-                                className='input input-bordered w-full bg-white text-black'
-                                value={nameLead2}
-                                onChange={(event) =>
-                                    setNameLead2(event.target.value)
-                                }
-                            />
-
-                            <input
-                                type='email'
-                                name='email'
-                                placeholder='E-mail'
-                                className='w-full text-black bg-white input input-bordered input-primary'
-                                value={emailLead2}
-                                onChange={(event) => setEmailLead2(event.target.value)}
-                            />
-
-                            <InputMask
-                                mask='(__) _____-____'
-                                replacement={{ _: /\d/ }}
-                                value={whatsappLead2}
-                                name='whatsapp_lead'
-                                onChange={handleInputChangeLead}
-                                className='input input-bordered w-full bg-white text-black'
-                                placeholder='(XX) 9XXXX-XXXX'
-                            />
-
-                            <div className='text-sm leading-4 text-white py-4'>
-                                Dentro da Agência Home Office você tem acesso completo a um ecossistema para
-                                criação da sua Agência de Viagens Online, contando com sistema de gestão próprio
-                                e acompanhamento personalizado.<br /><br />
-                                Já somos mais de 1.200 agências em todo Brasil que somam mais de 50 milhões de
-                                faturamento.<br /><br />
-                                Considerando a entrega proposta, qual valor você tem para investir em sua
-                                agência?
-                            </div>
-                            <select
-                                name='renda'
-                                className='w-full text-black bg-white select select-bordered select-primary'
-                                onChange={(event) =>
-                                    setRenda(event.target.value)
-                                }
+                            <form
+                                id='formLead'
+                                className='py-4 grid gap-2'
+                                onSubmit={handleSubmitLead}
                             >
-                                <option value='' selected disabled>
-                                    Valor disponível para investimento
-                                </option>
-                                <option value='Até 5k'>
-                                    de R$ 3.000 a R$5.000,00
-                                </option>
-                                <option value='De 5k a 8k'>
-                                    De R$5.000,00 a R$8.000,00
-                                </option>
-                                <option value='De 8k a 12k'>
-                                    De R$8.000,00 a R$12.000,00
-                                </option>
-                                <option value='Acima de 12k'>
-                                    Acima de R$12.000,00
-                                </option>
-                            </select>
-                        </form>
-                    </div>
-                </div>
-                <button
-                    form='formLead'
-                    type='submit'
-                    className='btn rounded-md btn-block btn-primary border-0 animation animate-pulse hover:bg-white hover:text-primary box-glow'
-                    disabled={loading2}
-                >
-                    {loading2 && (
-                        <svg
-                            className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
-                            xmlns='http://www.w3.org/2000/svg'
-                            fill='none'
-                            viewBox='0 0 24 24'
-                        >
-                            <circle
-                                className='opacity-25'
-                                cx='12'
-                                cy='12'
-                                r='10'
-                                stroke='currentColor'
-                                strokeWidth='4'
-                            ></circle>
-                            <path
-                                className='opacity-75'
-                                fill='currentColor'
-                                d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
-                            ></path>
-                        </svg>
-                    )}
+                                <input
+                                    type='text'
+                                    name='name_lead'
+                                    placeholder='Nome Completo'
+                                    className='input input-bordered w-full bg-white text-black'
+                                    value={nameLead2}
+                                    onChange={(event) =>
+                                        setNameLead2(event.target.value)
+                                    }
+                                />
 
-                    Enviar
-                </button>
+                                <input
+                                    type='email'
+                                    name='email'
+                                    placeholder='E-mail'
+                                    className='w-full text-black bg-white input input-bordered input-primary'
+                                    value={emailLead2}
+                                    onChange={(event) => setEmailLead2(event.target.value)}
+                                />
+
+                                <InputMask
+                                    mask='(__) _____-____'
+                                    replacement={{ _: /\d/ }}
+                                    value={whatsappLead2}
+                                    name='whatsapp_lead'
+                                    onChange={handleInputChangeLead}
+                                    className='input input-bordered w-full bg-white text-black'
+                                    placeholder='(XX) 9XXXX-XXXX'
+                                />
+
+                                <div className='text-sm leading-4 text-white py-4'>
+                                    Dentro da Agência Home Office você tem acesso completo a um ecossistema para
+                                    criação da sua Agência de Viagens Online, contando com sistema de gestão próprio
+                                    e acompanhamento personalizado.<br /><br />
+                                    Já somos mais de 1.200 agências em todo Brasil que somam mais de 50 milhões de
+                                    faturamento.<br /><br />
+                                    Considerando a entrega proposta, qual valor você tem para investir em sua
+                                    agência?
+                                </div>
+                                <select
+                                    name='renda'
+                                    className='w-full text-black bg-white select select-bordered select-primary'
+                                    onChange={(event) =>
+                                        setRenda(event.target.value)
+                                    }
+                                >
+                                    <option value='' selected disabled>
+                                        Valor disponível para investimento
+                                    </option>
+                                    <option value='Até 5k'>
+                                        de R$ 3.000 a R$5.000,00
+                                    </option>
+                                    <option value='De 5k a 8k'>
+                                        De R$5.000,00 a R$8.000,00
+                                    </option>
+                                    <option value='De 8k a 12k'>
+                                        De R$8.000,00 a R$12.000,00
+                                    </option>
+                                    <option value='Acima de 12k'>
+                                        Acima de R$12.000,00
+                                    </option>
+                                </select>
+                            </form>
+                        </div>
+                    </div>
+                    <button
+                        form='formLead'
+                        type='submit'
+                        className='btn rounded-md btn-block btn-primary border-0 animation animate-pulse hover:bg-white hover:text-primary box-glow'
+                        disabled={loading2}
+                    >
+                        {loading2 && (
+                            <svg
+                                className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
+                                xmlns='http://www.w3.org/2000/svg'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                            >
+                                <circle
+                                    className='opacity-25'
+                                    cx='12'
+                                    cy='12'
+                                    r='10'
+                                    stroke='currentColor'
+                                    strokeWidth='4'
+                                ></circle>
+                                <path
+                                    className='opacity-75'
+                                    fill='currentColor'
+                                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                                ></path>
+                            </svg>
+                        )}
+
+                        Enviar
+                    </button>
+                </div>
             </div>
-        </div>
+        ) : (
+            <div className={'bg-white h-screen'}>
+                <ToastContainer />
+                <div className='w-full max-w-2xl text-left flex flex-col mx-auto'>
+                    <h2 className='font-light text-black mt-4 text-xs sm:text-base'>
+                        Preencha seus dados abaixo para receber a apresentação completa da <span
+                        className='font-bold'>Agência de Viagens Home Office</span> ✌
+                    </h2>
+                    <div className='card card-compact border w-full h-fit rounded-lg my-4 md:my-12'>
+                        <div className='card-body'>
+                            <div className='card-title text-primary'>Inscreva-se</div>
+
+                            <form
+                                id='formLead'
+                                className='py-4 grid gap-2'
+                                onSubmit={handleSubmitLead}
+                            >
+                                <input
+                                    type='text'
+                                    name='name_lead'
+                                    placeholder='Nome Completo'
+                                    className='input input-bordered w-full bg-white text-black'
+                                    value={nameLead2}
+                                    onChange={(event) =>
+                                        setNameLead2(event.target.value)
+                                    }
+                                />
+
+                                <input
+                                    type='email'
+                                    name='email'
+                                    placeholder='E-mail'
+                                    className='w-full text-black bg-white input input-bordered input-primary'
+                                    value={emailLead2}
+                                    onChange={(event) => setEmailLead2(event.target.value)}
+                                />
+
+                                <InputMask
+                                    mask='(__) _____-____'
+                                    replacement={{ _: /\d/ }}
+                                    value={whatsappLead2}
+                                    name='whatsapp_lead'
+                                    onChange={handleInputChangeLead}
+                                    className='input input-bordered w-full bg-white text-black'
+                                    placeholder='(XX) 9XXXX-XXXX'
+                                />
+
+                                <div className='text-sm leading-4 text-black py-4'>
+                                    Dentro da Agência Home Office você tem acesso completo a um ecossistema para
+                                    criação da sua Agência de Viagens Online, contando com sistema de gestão próprio
+                                    e acompanhamento personalizado.<br /><br />
+                                    Já somos mais de 1.200 agências em todo Brasil que somam mais de 50 milhões de
+                                    faturamento.<br /><br />
+                                    Considerando a entrega proposta, qual valor você tem para investir em sua
+                                    agência?
+                                </div>
+                                <select
+                                    name='renda'
+                                    className='w-full text-black bg-white select select-bordered select-primary'
+                                    onChange={(event) =>
+                                        setRenda(event.target.value)
+                                    }
+                                >
+                                    <option value='' selected disabled>
+                                        Valor disponível para investimento
+                                    </option>
+                                    <option value='Até 5k'>
+                                        de R$ 3.000 a R$5.000,00
+                                    </option>
+                                    <option value='De 5k a 8k'>
+                                        De R$5.000,00 a R$8.000,00
+                                    </option>
+                                    <option value='De 8k a 12k'>
+                                        De R$8.000,00 a R$12.000,00
+                                    </option>
+                                    <option value='Acima de 12k'>
+                                        Acima de R$12.000,00
+                                    </option>
+                                </select>
+                            </form>
+                        </div>
+                    </div>
+                    <button
+                        form='formLead'
+                        type='submit'
+                        className='btn rounded-md btn-block btn-primary border-0 animation animate-pulse hover:bg-white hover:text-primary box-glow'
+                        disabled={loading2}
+                    >
+                        {loading2 && (
+                            <svg
+                                className='animate-spin -ml-1 mr-3 h-5 w-5 text-white'
+                                xmlns='http://www.w3.org/2000/svg'
+                                fill='none'
+                                viewBox='0 0 24 24'
+                            >
+                                <circle
+                                    className='opacity-25'
+                                    cx='12'
+                                    cy='12'
+                                    r='10'
+                                    stroke='currentColor'
+                                    strokeWidth='4'
+                                ></circle>
+                                <path
+                                    className='opacity-75'
+                                    fill='currentColor'
+                                    d='M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z'
+                                ></path>
+                            </svg>
+                        )}
+
+                        Enviar
+                    </button>
+                </div>
+            </div>
+        )
     )
 }
